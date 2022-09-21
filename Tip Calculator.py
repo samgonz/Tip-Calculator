b995c2fa21b8263eecdd90ff4069ef8b3c3c11de
samgonz <doomteam1@gmail.com>
2022-09-20 21:41:39 -0500

    added ablity to tip

--- Tip Calculator.py
+++ Tip Calculator.py
@@ -1,7 +1,16 @@
 def tipQuestions():
     billTotal = input('What is the bill total?\n')
     numberOfPeople = input('How many people will the bill be split between?\n')
+    tipQuestion = input('Would you like to give a tip? (Y or N)\n')
+    
+    if tipQuestion.upper() == 'Y':
+        tipPercentQuestion = input('How much of a tip would you like to give?\n')
+        tipPercent = float(tipPercentQuestion) * .01
+        tipAmount = float(billTotal) * tipPercent
+        costPerPerson = (float(billTotal) + tipAmount) / int(numberOfPeople)
+    else:
+        costPerPerson = float(billTotal) / int(numberOfPeople)
 
-    return print('Total cost will be', "${:,.2f}".format(float(billTotal) / int(numberOfPeople)), 'per person.')
+    return print('Total cost will be', "${:,.2f}".format(costPerPerson), 'per person.')
 
 tipQuestions()
